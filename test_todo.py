288e939ac066da5895a70f67c0833d476c44abbf
Stoleroff <nutselectro@gmail.com>
2025-11-17 21:50:43 +0300

    Новая проверка

--- test_todo.py
+++ test_todo.py
@@ -8,3 +8,10 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
 
+def test_loc(page):
+    page.goto('https://zimaev.github.io/text_input/')
+    page.get_by_label("Email address").fill("qa@example.com")
+    page.get_by_title("username").fill("Anton")
+    page.get_by_placeholder('password').fill("secret")
+    page.get_by_role('checkbox').click()
+
